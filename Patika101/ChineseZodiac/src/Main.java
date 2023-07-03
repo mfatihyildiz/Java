f7b6135879e5a286173e25412dc96510f029f815
Mehmet Fatih Yıldız <93758135+mfatihyildiz@users.noreply.github.com>
2023-07-03 17:23:13 +0300

    Add files via upload

---
+++ Patika101/ChineseZodiac/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+
+        int year,mod;
+        System.out.print("Enter Your Birth Year: ");
+        year=inp.nextInt();
+
+        mod=year%12;
+
+        switch (mod){
+            case 0:
+                System.out.println("Monkey");
+                break;
+            case 1:
+                System.out.println("Rooster");
+                break;
+            case 2:
+                System.out.println("Dog");
+                break;
+            case 3:
+                System.out.println("Pig");
+                break;
+            case 4:
+                System.out.println("Rat");
+                break;
+            case 5:
+                System.out.println("Ox");
+                break;
+            case 6:
+                System.out.println("Tiger");
+                break;
+            case 7:
+                System.out.println("Rabbit");
+                break;
+            case 8:
+                System.out.println("Dragon");
+                break;
+            case 9:
+                System.out.println("Snake");
+                break;
+            case 10:
+                System.out.println("Horse");
+                break;
+            case 11:
+                System.out.println("Goat");
+                break;
+
+        }
+    }
+}
